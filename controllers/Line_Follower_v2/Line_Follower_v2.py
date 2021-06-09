4c34df5d3e49709d0e0cf823312956520f3c5352
Texasushka <37118296+Texasushka@users.noreply.github.com>
2021-06-09 19:39:11 +0200

    Add files via upload

--- controllers/Line_Follower_v2/Line_Follower_v2.py
+++ controllers/Line_Follower_v2/Line_Follower_v2.py
@@ -1,24 +1,30 @@
 """Line_Follower_v1 controller."""
+"""Importowanie biblioteki applikacji Webots z funkcjami dla kontroli robota
+    Zakomentowałem biblioteki """
 from controller import Camera
 from controller import CameraRecognitionObject
 from controller import Robot
 
+"""Funkcja z kodem dla opracowania działania robota"""
 def run_robot(robot):
     time_step = 32
     default_speed = 1
     max_speed = 1
-    #Motors
+    """Włączenie motorów kołek robota"""
     left_motor = robot.getDevice('left wheel motor')
     right_motor = robot.getDevice('right wheel motor')
     left_motor.setPosition(float('inf'))
     right_motor.setPosition(float('inf'))
     left_motor.setVelocity(0)
     right_motor.setVelocity(0)
-    
+
+    """Włączenie czujników robota"""
     IR_left = robot.getDevice('IR_left')
     IR_left.enable(time_step)
     IR_right = robot.getDevice('IR_right')
     IR_right.enable(time_step)
+
+    """Włączenie kamery robota"""
     cam = robot.getDevice('camera')
     cam.enable(time_step)
     geti = -1
@@ -26,27 +32,30 @@ def run_robot(robot):
     width = cam.getWidth()
     height = cam.getHeight()
 
-    #Step simulation
+    """Symulacja czasu"""
     while robot.step(time_step)!= -1:
         
         
-        #read sensors
-        IR_left_value = IR_left.getValue()
-        IR_right_value = IR_right.getValue()
-        
-        image = cam.getImageArray()
-        left_sum = 0
-        right_sum = 0  
-       
-        for x in range(0,25):
+       """Odczytywanie wartości czujników"""
+       IR_left_value = IR_left.getValue()
+       IR_right_value = IR_right.getValue()
+
+       """Odczytanie obrazu kamery"""
+       image = cam.getImageArray()
+       left_sum = 0
+       right_sum = 0
+
+       """Analiza pikseli pobranego obrazku i wyliczzanie szarości każdej połówy"""
+       """Prawej"""
+       for x in range(0,25):
           for y in range(20,30):
             red   = image[x][y][0]
             green = image[x][y][1]
             blue  = image[x][y][2]
             gray  = (red + green + blue) / 3
             left_sum += gray
-            
-        for x in range(26,52):
+       """Oraz lewej"""
+       for x in range(26,52):
            for y in range(20,30):
                     red   = image[x][y][0]
                     green = image[x][y][1]
@@ -54,70 +63,72 @@ def run_robot(robot):
                     gray  = (red + green + blue) / 3
                     right_sum += gray
             
-       
-        left_sum = left_sum / 1000
-        right_sum = right_sum / 1000
-        
-        if ( right_sum < 30 or left_sum < -30):
-                max_speed = default_speed * 1.5
-        elif ( right_sum < 20 or left_sum < 20):
-                max_speed = default_speed * 0.3
-        elif ( right_sum > 40 or left_sum > 40):
-                max_speed = default_speed * 3.5
-        else:
+       """Zmniejszenie wyliczonej wartości dla wygodnego korzystania"""
+       left_sum = left_sum / 1000
+       right_sum = right_sum / 1000
+
+       """Kontrola szybkości w zależności od wyniku obliczeń szarości"""
+       if ( right_sum < 30 or left_sum < -30):
+               max_speed = default_speed * 1.5
+       elif ( right_sum < 20 or left_sum < 20):
+               max_speed = default_speed * 0.3
+       elif ( right_sum > 40 or left_sum > 40):
+               max_speed = default_speed * 3.5
+       else:
             max_speed = default_speed
-                 
-        
-       # print (max_speed)
+
        
-       
-        if(IR_left_value > 5 and IR_right_value < 5):
+       """Analizując wartości otrzymane od czujników oraz średnią szarość 
+       otrzymaną po analizie obrazu z kamery
+       program decyduje czy robot musi skrecać"""
+       if(IR_left_value > 5 and IR_right_value < 5):
             x = 0
             left_speed = max_speed  *0.0
             right_speed = max_speed  * 1.7
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
             x += 1
-        elif(IR_right_value > 5 and IR_left_value < 5 ):
+       elif(IR_right_value > 5 and IR_left_value < 5 ):
             x = 0
             left_speed = max_speed  * 1.7
             right_speed = max_speed * 0.0
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
             x += 1
-        elif(IR_right_value == 1000 and IR_left_value  == 1000):
+       elif(IR_right_value == 1000 and IR_left_value  == 1000):
             left_speed = max_speed  * 1.7
             right_speed = max_speed * 0.0
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
-        elif(IR_right_value >5 and IR_left_value >5  and left_sum <80 and right_sum < 80):
+       elif(IR_right_value >5 and IR_left_value >5  and left_sum <80 and right_sum < 80):
             left_speed = max_speed
             right_speed = max_speed
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
-        elif(left_sum >110 and right_sum > 110):
+       elif(left_sum >110 and right_sum > 110):
             left_speed = max_speed  * 1.7
             right_speed = max_speed * 0.0
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
-        elif(IR_right_value >5 and IR_left_value >5  and left_sum > right_sum):
+       elif(IR_right_value >5 and IR_left_value >5  and left_sum > right_sum):
             left_speed = max_speed  * 1.7
             right_speed = max_speed * 0.0
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
-        elif(IR_right_value >5 and IR_left_value >5 and left_sum < right_sum):
+       elif(IR_right_value >5 and IR_left_value >5 and left_sum < right_sum):
             left_speed =max_speed * 0.0
             right_speed =  max_speed  * 1.7
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
                       
-        else:
+       else:
             left_speed = max_speed
             right_speed = max_speed
-        left_motor.setVelocity(left_speed)
-        right_motor.setVelocity(right_speed)          
-            
-        
+       left_motor.setVelocity(left_speed)
+       right_motor.setVelocity(right_speed)
+
+
+"""Funkcja 'main' włącza robota oraz wywołuje funkcje 'run_robot'"""
 if __name__ == "__main__":
     my_robot = Robot()
     run_robot(my_robot)
